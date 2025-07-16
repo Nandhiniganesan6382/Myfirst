e00bfe48f221f4c488cb0c64e1e394450dad233d
Nandhiniganesan6382 <nandhiniganesannandhiniganesh@gmail.com>
2025-07-16 21:55:11 +0530

    Add files via upload

---
+++ day16task1loops.js
@@ -0,0 +1,17 @@
+for(let i=1; i<=10; i++)
+{
+    console.log(i);
+}
+
+let y=1;
+while(y<=10)
+{
+    console.log("The use of while loop",y);
+    y++;
+}
+let x=10;
+do{
+    console.log("number is",x);
+    x++;
+
+}while(x<=10);
