807329cfeaee79c26de99bb00b25eebfc32a5692
Nandhiniganesan6382 <nandhiniganesannandhiniganesh@gmail.com>
2025-07-16 21:56:16 +0530

    Add files via upload

---
+++ day16task3reverseloop.js
@@ -0,0 +1,17 @@
+for(let i=10; i >= 1; i--)
+{
+    console.log(i);
+}
+
+let y=10;
+while(y >= 1)
+{
+    console.log("The use of while loop",y);
+    y--;
+}
+let x=10;
+do{
+    console.log("number is",x);
+    x--;
+
+}while(x >=1 );
